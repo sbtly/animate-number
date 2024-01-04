de3e6fec3a5eb3d2f6cb2c52d506d6611d38d31c
deploy <deploy@users.noreply.github.com>
2024-01-04 13:23:21 +0000

    Updated

---
+++ esmbuild@1.0.20/Button.js
@@ -0,0 +1,37 @@
+// src/Button.tsx
+import {
+  createElement
+} from "react";
+
+// esbuild-css-modules-plugin-namespace:/tmp/tmp-2008-pDFCcoy9Ia47/main/src/Button.modules.css.js
+var digest = "36deaa39e9880f45994f31af0400309679c4db567f7bd79a957697adced7b5dd";
+var css = `._button_wrfdr_1 {
+    background-color: #c8d5ff;
+}
+`;
+(function() {
+  if (!document.getElementById(digest)) {
+    var ele = document.createElement("style");
+    ele.id = digest;
+    ele.textContent = css;
+    document.head.appendChild(ele);
+  }
+})();
+var Button_modules_css_default = { "button": "_button_wrfdr_1" };
+
+// src/Button.tsx
+import { addPropertyControls, ControlType } from "framer";
+var InnerButton = (props) => {
+  return /* @__PURE__ */ createElement("button", {
+    className: Button_modules_css_default.button
+  }, props.title);
+};
+var Button = InnerButton;
+addPropertyControls(Button, {
+  title: {
+    type: ControlType.String
+  }
+});
+export {
+  Button
+};
---
+++ esmbuild@1.0.20/index.js
@@ -0,0 +1,13 @@
+// src/index.tsx
+import { Button } from "./Button.js";
+import { AnimateNumber } from "./AnimateNumber.js";
+import { AnimateNumber2 } from "./AnimateNumber2.js";
+import { Battery } from "./Battery.js";
+import { MotionButton } from "./MotionButton.js";
+export {
+  AnimateNumber,
+  AnimateNumber2,
+  Battery,
+  Button,
+  MotionButton
+};
